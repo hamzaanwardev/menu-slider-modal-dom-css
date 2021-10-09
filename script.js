26c1886a3b6164ac03f58593fea50db12765748e
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-10-09 23:57:49 +0500

    Completed

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// making Variables
 const toggle = document.getElementById('toggle');
+
 const close = document.getElementById('close');
 const open = document.getElementById('open');
 const modal = document.getElementById('modal');
